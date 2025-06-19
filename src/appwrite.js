76308ffc96f95d8f39be67854583bf9a76576c8d
Rushi-04 <borkarananta028@gmail.com>
2025-06-19 21:03:04 +0530

    reloaded

--- src/appwrite.js
+++ src/appwrite.js
@@ -3,13 +3,11 @@ import { Client, Databases, ID, Query } from 'appwrite';
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
-const API_KEY = import.meta.env.VITE_APPWRITE_API_KEY;
 
 //Appwrite Client Initialization
 const client = new Client()
     .setEndpoint('https://fra.cloud.appwrite.io/v1')
-    .setProject(PROJECT_ID)
-    .setKey(API_KEY);
+    .setProject(PROJECT_ID);
 
 
 const database = new Databases(client);
